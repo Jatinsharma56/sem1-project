b18014a952a807257cbd3cfca1ccc47b02ef563d
Jatin Sharma <jatinsharma00877@gmail.com>
2026-07-13 00:22:36 +0530

    BMW Hub - Sem 1 Project

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper(".portfolioSwiper", {
+
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+  },
+
+});
